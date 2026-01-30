b9473ebd60e8c65e5f33b976cdd6fbb861b9c5e3
MuchPlayer9 <tylerbrett08@gmail.com>
2026-01-30 12:39:41 +1030

    Added item Calculator and gave it a name

--- src/main/java/net/mp9/smelterizing/BetterSmelting.java
+++ src/main/java/net/mp9/smelterizing/BetterSmelting.java
@@ -1,5 +1,7 @@
 package net.mp9.smelterizing;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.mp9.smelterizing.item.ModItems;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
@@ -34,6 +36,8 @@ public class BetterSmelting {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,7 +50,9 @@ public class BetterSmelting {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.CALCULATOR);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/mp9/smelterizing/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.mp9.smelterizing.item;
+
+import net.minecraft.world.item.Item;
+import net.mp9.smelterizing.BetterSmelting;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BetterSmelting.MOD_ID);
+
+    public static final DeferredItem<Item> CALCULATOR = ITEMS.register("calculator",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
